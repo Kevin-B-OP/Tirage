41ff1b7467b5393b7ae85a3f19676a5e77d1bb91
kevin <kevin.bony@protonmail.com>
2019-05-08 18:26:41 +0200

    Ajout d'un menu mais problème pour la boucle de l'application

--- src/com/opckev/Nbdepersonne.java
+++ src/com/opckev/Nbdepersonne.java
@@ -3,9 +3,9 @@ package com.opckev;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Nbdepersonne {
+class Nbdepersonne {
     private Scanner sc = new Scanner(System.in);
-    public void PartieUn () {
+    void PartieUn () {
         // Choix du nombre de personne
         System.out.println("Combien de participants êtes vous ?");
         int people = -1;
@@ -14,9 +14,8 @@ public class Nbdepersonne {
         do {
             try {
                 people = sc.nextInt();
-                responseIsGood=true;
-                }
-            catch (InputMismatchException e){
+                responseIsGood = true;
+            } catch (InputMismatchException e) {
                 sc.next();
                 System.out.println("Vous devez saisir un nombre, correspondant au nombre de partcipants souhaités");
                 responseIsGood = false;
@@ -31,27 +30,43 @@ public class Nbdepersonne {
             String name = sc.next();
             tableau[i] = name;
         }
-        // Tirage manuel avec une vérification de chiffre
-        System.out.println("Veuillez choisir un chiffre entre 1 et " + people);
-        int choix =-1;
-        boolean response;
-        do {
-            try {
-                choix = sc.nextInt();
-                response = true;
-            } catch (InputMismatchException e) {
-                sc.next();
-                System.out.println("Vous devez saisir un nombre, correspondant au nombre de partcipants souhaités");
-                response = false;
+            homepage();
+            int nbresponse;
+            nbresponse = sc.nextInt();
+            if (nbresponse == 1) {
+                // Tirage manuel avec une vérification de chiffre
+                System.out.println("Veuillez choisir un chiffre entre 1 et " + people);
+                int choix = -1;
+                boolean response;
+                do {
+                    try {
+                        choix = sc.nextInt();
+                        response = true;
+                    } catch (InputMismatchException e) {
+                        sc.next();
+                        System.out.println("Vous devez saisir un nombre, correspondant au nombre de partcipants souhaités");
+                        response = false;
+                    }
+                }
+                while (!response);
+                System.out.println("La personne que vous avez choisie est " + tableau[choix - 1]);
+                homepage();
+            } else if (nbresponse == 2) {
+                // Tirage aléatoire
+                int lower = 0;
+                int random = (int) (Math.random() * (people - lower)) + lower;
+                System.out.println("Le gagnant aléatoire est " + tableau[random]);
+                homepage();
+            } else {
+                System.out.println("Au revoir");
             }
         }
-        while (!response);
-        System.out.println("La personne que vous avez choisie est "+tableau[choix-1]);
-        // Tirage aléatoire
-        int lower = 0;
-        int higher = people;
-        int random = (int)(Math.random() * (higher-lower)) + lower;
-        System.out.println("Le gagnant aléatoire est "+tableau[random]);
+    private void homepage () {
+        System.out.println("Que souhaitez vous faire maintenant ?");
+        System.out.println("1 - Un tirage manuel");
+        System.out.println("2 - Un tirage aléatoire");
+        System.out.println("3 - Arrêter l'application");
+
     }
 }
 
