643ff8af165f73497af196b84f3a0fb8d096bb7c
kevin <kevin.bony@protonmail.com>
2019-05-12 13:10:21 +0200

    Mise en place d'un troisième fonctionnement qui intègre une boucle et la possibilité de changer les participants. L'appli fonctionne comme je le voulais avec la version 3.

---
+++ src/com/opckev/Fonctionnement3/Appli.java
@@ -0,0 +1,79 @@
+package com.opckev.Fonctionnement3;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+class Appli {
+    private Scanner sc = new Scanner(System.in);
+
+    void PartieUn() {
+        // Choix du nombre de personne
+        int people = -1;
+        // Permet de catcher une erreur si jamais la personne rentre une lettre
+        boolean responseIsGood;
+        do {
+            System.out.println("Combien de participants êtes vous ?");
+            try {
+                people = sc.nextInt();
+                responseIsGood = true;
+            } catch (InputMismatchException e) {
+                sc.next();
+                System.out.println("Vous devez saisir un nombre, correspondant au nombre de partcipants souhaités");
+                responseIsGood = false;
+            }
+        }
+        while (!responseIsGood);
+        String[] tableau = new String[people];
+        // Boucle pour donner un nom à chaque participant
+        for (int i = 0; i < people; i++) {
+            int nb = i + 1;
+            System.out.println("Veuillez donner le nom du participant " + nb);
+            String name = sc.next();
+            tableau[i] = name;
+        }
+        boolean continuer;
+        do {
+            System.out.println("Que souhaitez vous faire maintenant ?");
+            System.out.println("1 - Un tirage manuel");
+            System.out.println("2 - Un tirage aléatoire");
+            System.out.println("3 - Changer les participants");
+            System.out.println("4 - Arrêter l'application");
+            int nbresponse;
+            nbresponse = sc.nextInt();
+            if (nbresponse == 1) {
+                // Tirage manuel avec une vérification de chiffre
+                System.out.println("Veuillez choisir un chiffre entre 1 et " + people+"\n");
+                int choix = -1;
+                boolean response;
+                do {
+                    try {
+                        choix = sc.nextInt();
+                        response = true;
+                    } catch (InputMismatchException e) {
+                        sc.next();
+                        System.out.println("Vous devez saisir un nombre, correspondant au nombre de partcipants souhaités"+"\n");
+                        response = false;
+                    }
+                }
+                while (!response);
+                System.out.println("La personne que vous avez choisie est " + tableau[choix - 1]+"\n");
+                continuer = true;
+            } else if (nbresponse == 2) {
+                // Tirage aléatoire
+                int lower = 0;
+                int random = (int) (Math.random() * (people - lower)) + lower;
+                System.out.println("Le gagnant aléatoire est " + tableau[random]+"\n");
+                continuer = true;
+            } else if (nbresponse == 3) {
+                PartieUn();
+                continuer = false;
+            }
+            else {
+                System.out.println("Au revoir");
+                continuer = false;
+            }
+        }
+        while (continuer);
+    }
+}
+
---
+++ src/com/opckev/Fonctionnement3/Main.java
@@ -0,0 +1,11 @@
+package com.opckev.Fonctionnement3;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Appli loterie = new Appli();
+        loterie.PartieUn();
+    }
+}
